73e321f6237c9202d7bd1c1259773c46a0ae7e0d
D'elisa Giuseppe <dbga_56@dbga.local>
2017-07-05 13:55:38 +0200

    Movement player

--- Assets/Scripts/HeadAnimation.cs
+++ Assets/Scripts/HeadAnimation.cs
@@ -6,10 +6,15 @@ public class HeadAnimation : MonoBehaviour {
 
     Animator anim;
     Transform firePosition;
+    Rigidbody2D playerRB;
+    float nextFire = 0.0F;
     public GameObject bullet;
     public float bulletVelocity;
-    public float bulletrange;
-    
+    public float bulletRange;
+    public float bulletRate;
+    float previousH = 0;
+    float previousV = 0;
+
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
@@ -18,43 +23,74 @@ public class HeadAnimation : MonoBehaviour {
 
     // Update is called once per frame
     void Update() {
+        playerRB = GetComponentInParent<Rigidbody2D>();
         float h = Input.GetAxisRaw("Horizontal2");
         float v = Input.GetAxisRaw("Vertical2");
-        bool walking = h != 0f || v != 0f;
-        anim.SetBool("isWalking", walking);
-        if(walking)
+        float h1 = Input.GetAxisRaw("Horizontal");
+        float v1 = Input.GetAxisRaw("Vertical");
+
+        bool shooting = h != 0f || v != 0f;
+        bool moving = h1 != 0f || v1 != 0f;
+
+        anim.SetBool("isShooting", shooting);
+        anim.SetBool("isMoving", moving);
+
+        float speedPlayerX = playerRB.velocity.x != 0 ? playerRB.velocity.x : 0;
+        float speedPlayerY = playerRB.velocity.y != 0 ? playerRB.velocity.y : 0;
+        float xSpeed = bulletVelocity + speedPlayerX;//Mathf.Abs(maxSpeed * Mathf.Cos(throwAngle));
+        float ySpeed = bulletVelocity + speedPlayerY;// Mathf.Abs(maxSpeed * Mathf.Sin(throwAngle));
+
+        if (h > 0)
         {
+            ySpeed = bulletRange;
+        }
+        if (h < 0)
+        {
+            xSpeed = -xSpeed;
+            ySpeed = bulletRange;
+        }
+        else if (v > 0)
+        {
+            xSpeed = 0;
+        }
+        else if (v < 0 || (h == 0 && v == 0))
+        {
+            xSpeed = 0;
+            ySpeed = -bulletRange;
+        }
+
+        if(moving)
+        {
+            previousH = h1;
+            previousV = v1;
+            anim.SetFloat("movementHorizontal", h1);
+            anim.SetFloat("movementVertical", v1);
+        }
+        else
+        {
+            anim.SetFloat("movementHorizontal", previousH);
+            anim.SetFloat("movementVertical", previousV);
+        }
+
+        if (shooting)
+        {
+            previousH = h;
+            previousV = v;
             anim.SetFloat("speedHorizontal", h);
             anim.SetFloat("speedVertical", v);
+            if(Time.time > nextFire)
+            {
+                nextFire = Time.time + bulletRate;
+                Fire(xSpeed, ySpeed);
+            }
         }
-
-        if (Input.GetKeyDown(KeyCode.Space))
+        else
         {
-
-            float xSpeed = bulletVelocity;//Mathf.Abs(maxSpeed * Mathf.Cos(throwAngle));
-            float ySpeed = bulletVelocity;// Mathf.Abs(maxSpeed * Mathf.Sin(throwAngle));
-
-            if (h > 0)
-            {
-                ySpeed = bulletrange;
-            }
-            if (h < 0)
-            {
-                xSpeed = -xSpeed;
-                ySpeed = bulletrange;
-            }
-            else if (v > 0)
-            {
-                xSpeed = 0;
-            }
-            else if (v < 0 || (h == 0 && v == 0))
-            {
-                xSpeed = 0;
-                ySpeed = -bulletrange;
-            }
-            
-            Fire(xSpeed, ySpeed);
+            anim.SetFloat("movementHorizontal", previousH);
+            anim.SetFloat("movementVertical", previousV);
         }
+
+
     }
 
     void Fire(float xSpeed, float ySpeed)
--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -6,15 +6,16 @@ public class PlayerMovement : MonoBehaviour {
 
     public float speed = 5f;
     public GameObject bullet;
-    
+    Rigidbody2D playerRB;
 
 	// Use this for initialization
 	void Start () {
-        
+        playerRB = GetComponent<Rigidbody2D>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         transform.Translate(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime, 0f);
+        playerRB.velocity = new Vector2(Input.GetAxis("Horizontal") * speed * Time.deltaTime, Input.GetAxis("Vertical") * speed * Time.deltaTime);
 	}
 }
