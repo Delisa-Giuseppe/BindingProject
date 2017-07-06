0db0b8d6798ca7cab8ad2671b40c3a4a53f9e86b
D'elisa Giuseppe <dbga_56@dbga.local>
2017-07-06 14:16:13 +0200

    Enemy IA

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -4,8 +4,21 @@ using UnityEngine;
 
 public class BulletControl : MonoBehaviour {
 
+    Rigidbody2D bulletRB;
+
+    private void Start()
+    {
+        bulletRB = GetComponent<Rigidbody2D>();
+    }
+
+
     void OnCollisionEnter2D(Collision2D coll)
     {
+        if(coll.gameObject.tag == "Enemy")
+        {
+            coll.gameObject.GetComponent<EnemyIA>().health = coll.gameObject.GetComponent<EnemyIA>().health - 1;
+        }
+
         Destroy(gameObject);
     }
 
---
+++ Assets/Scripts/ChaseEnemy.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseEnemy : EnemyIA {
+
+    public GameObject target;
+    public float moveSpeed;
+
+    Vector2 currentDirection;
+    Vector2 direction;
+    bool move = true;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (move)
+        {
+            direction = (target.transform.position - transform.position).normalized;
+        }
+        currentDirection = direction;
+        transform.Translate(direction * moveSpeed * Time.deltaTime);
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            move = false;
+
+            if (currentDirection == Vector2.left)
+            {
+                direction = Vector2.up;
+            }
+            else if (currentDirection == Vector2.right)
+            {
+                direction = Vector2.up;
+            }
+            else if (currentDirection == Vector2.up)
+            {
+                direction = Vector2.left;
+            }
+            else if (currentDirection == Vector2.down)
+            {
+                direction = Vector2.right;
+            }
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Obstacle")
+        {
+            move = true;
+        }
+    }
+
+}
