d195961236eb8a900d870a3b2bbb8afc8a870705
AndreaV96 <andrea.viglietti96@gmail.com>
2017-07-09 21:08:58 +0200

    Ho messo sia la scena del boss ma la'ho anche copiata nella Scena level...Ora i proiettili non vanno avanti...

---
+++ Assets/Scripts/Boss.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss : MonoBehaviour {
+    public Transform target;
+    public GameObject projectile;
+    public Transform shootPoint;
+    public float shootCd;
+    float nextFire = 0;
+    public float health = 100f;
+	// Use this for initialization
+
+
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if(Time.time > nextFire)
+        {
+            nextFire = Time.time + shootCd;
+            Fire();
+        }
+            
+        if(health <= 50)
+        {
+
+        }
+        
+	}
+
+    public void Fire()
+    {
+        //transform.LookAt(target);
+        GameObject bulletInstance = Instantiate(projectile, shootPoint.position, shootPoint.rotation);
+        GameObject bulletInstance1 = Instantiate(projectile, shootPoint.position, shootPoint.rotation);
+        bulletInstance.GetComponent<Rigidbody2D>().velocity = Vector3.up + (target.position - transform.position) * 1.5f;
+        bulletInstance1.GetComponent<Rigidbody2D>().velocity = Vector3.left + (target.position - transform.position) * 1.5f;
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Bullet")
+        {
+            health -= health - 20f;
+        }
+    }
+}
---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+    Transform target;
+    public Rigidbody2D myRigidBody;
+	// Use this for initialization
+	void Start () {
+        target = GameObject.FindGameObjectWithTag("Player").transform;
+        myRigidBody = GetComponent<Rigidbody2D>();
+        // myRigidBody.velocity = (target.position - transform.position) * 1.5f;
+        // myRigidBody.velocity = target.position * 1.5f* Time.deltaTime;
+       	 Destroy(gameObject, 2f);
+    }
+
+    // Update is called once per frame
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            Destroy(this.gameObject);
+        }
+        
+    }
+
+    private void Update()
+    {
+       
+    }
+}
