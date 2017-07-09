82fe8bf0e7c41b6609e0602042982d750d1afa0e
AndreaV96 <andrea.viglietti96@gmail.com>
2017-07-09 21:18:04 +0200

    Il Boss spara adesso

--- Assets/Scripts/Projectile.cs
+++ Assets/Scripts/Projectile.cs
@@ -3,12 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Projectile : MonoBehaviour {
-    Transform target;
-    public Rigidbody2D myRigidBody;
+	public Transform target;
+	public Rigidbody2D myRigidBody;
+    
 	// Use this for initialization
 	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D> ();
         target = GameObject.FindGameObjectWithTag("Player").transform;
-        myRigidBody = GetComponent<Rigidbody2D>();
+        
         // myRigidBody.velocity = (target.position - transform.position) * 1.5f;
         // myRigidBody.velocity = target.position * 1.5f* Time.deltaTime;
        	 Destroy(gameObject, 2f);
